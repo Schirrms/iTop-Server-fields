db47f3410c137e916a0025981d8c69e2398f47c5
Schirrms <schirrms@schirrms.net>
2019-08-07 16:30:55 +0200

    First upload on GitHub (version 1.1.2)

---
+++ schirrms-server-addfields/fr.dict.schirrms-server-addfields.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Localized data
+ *
+ * @copyright   Copyright (C) 2018-2019 Schirrms
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+Dict::Add('FR FR', 'French', 'Français', array(
+	// Dictionary entries go here
+	'Class:PhysicalDevice/Attribute:modelnumber' => 'Numéro de modèle',
+	'Class:Server/Attribute:modelnumber' => 'Numéro de Modèle',
+	'Class:Server/Attribute:S_UUID' => 'UUID du serveur',
+	'Class:Server/Attribute:S_Function' => 'Fonction du serveur',
+));
+?>
